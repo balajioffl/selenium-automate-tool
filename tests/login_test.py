120ff5672103cfbcfd2ed39c3950116e57690e9d
balajioffl <balajioffl07@gmail.com>
2026-02-05 14:14:40 +0530

    file added

---
+++ tests/login_test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+print("Page Title is:", driver.title)
+
+if "OrangeHRM" in driver.title:
+    print("Login Page Loaded - PASS")
+else:
+    print("Login Page Failed - FAIL")
+
+driver.quit()
