605646528c740172c207a1c0458511967e0f9b7e
balajioffl <balajioffl07@gmail.com>
2026-02-06 15:47:03 +0530

    Converted login test to pytest

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,19 +1,24 @@
-# TA-10: Verify Login Page Loads
-
+import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
 
-driver.get("https://opensource-demo.orangehrmlive.com/")
+def test_login_page_loads():
+    options = Options()
+    options.add_argument("--headless")   # Important for QATouch cloud run
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-print("Page Title is:", driver.title)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
 
-if "OrangeHRM" in driver.title:
-    print("Login Page Loaded - PASS")
-else:
-    print("Login Page Failed - FAIL")
+    driver.get("https://opensource-demo.orangehrmlive.com/")
 
-driver.quit()
+    title = driver.title
+    print("Page Title is:", title)
+
+    assert "OrangeHRM" in title, "Login Page Failed"
+
+    driver.quit()
